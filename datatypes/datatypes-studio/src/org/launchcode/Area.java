96310e33b7d6a7d7a6f218691e894ba0bd68bf78
hannahmihulka <hannahmihulka@gmail.com>
2023-08-21 19:41:59 -0500

    finished studio

---
+++ datatypes/datatypes-studio/src/org/launchcode/Area.java
@@ -0,0 +1,21 @@
+import org.launchcode.Circle;
+
+import java.util.Scanner;
+
+public class Area {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Please enter the radius of a circle: ");
+        double radius = scanner.nextDouble();
+        scanner.close();
+
+        double pi = 3.14159;
+        double area = pi * radius * radius;
+        System.out.println("The area of the circle is: " + area);
+
+
+        double circleArea = Circle.getArea(radius);
+        System.out.println("The area of a circle using Circle class is: " + circleArea);
+    }
+}
+
---
+++ datatypes/datatypes-studio/src/org/launchcode/Circle.java
@@ -0,0 +1,9 @@
+package org.launchcode;
+
+public class Circle {
+    public static Double getArea(Double radius) {
+        double pi = 3.14159;
+        double area = pi * radius * radius;
+        return area;
+    }
+}
